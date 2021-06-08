330b28ea211613bbc2729b2fad551b9676b773e7
Luke Arenas <lukedarenas@gmail.com>
2021-06-08 12:20:34 -0600

    set up server.js and connected mongodb

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const { graphQLHTTP } = require('express-graphql')
+const mongoose = require('mongoose')
+
+const app = express()
+
+app.use(bodyParser.json())
+
+// app.use('/graphql', graphQLHTTP({
+//   schema: ,
+//   rootValue: ,
+//   graphiql: true
+// }))
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@synchronize.bijfb.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(3001)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
