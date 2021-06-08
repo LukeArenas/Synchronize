12edf2092aeedc723a6c088c1cbea32290d56d22
Luke Arenas <lukedarenas@gmail.com>
2021-06-08 12:51:38 -0600

    created schema and models

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express()
 app.use(bodyParser.json())
 
 // app.use('/graphql', graphQLHTTP({
-//   schema: ,
+//   schema: graphQLSchema,
 //   rootValue: ,
 //   graphiql: true
 // }))
