178598efa5f50b369f5076f80338acf7c6ec3d0c
Luke Arenas <lukedarenas@gmail.com>
2021-06-09 17:55:26 -0600

    implemented logic to create array of dates for current month

--- client/src/pages/Calendar.js
+++ client/src/pages/Calendar.js
@@ -1,18 +1,37 @@
 import React from 'react'
 import Day from '../components/Day'
 
+//logic to find current month, year
+const today = new Date().toDateString()
+const date = new Date().toJSON().toString()
+const month = parseInt(date.slice(5, 7))
+const year = today.slice(11)
+
+//populating array with date objects
+let dateArray = []
+for (let i = 1; i < 31; i++) {
+  const nextDay = new Date(year, month - 1, i).toString()
+  let dayObj = {
+    weekday: nextDay.slice(0, 3),
+    date: nextDay.slice(8, 10),
+    month: month,
+    year: year
+  }
+  dateArray.push(dayObj)
+}
+console.log(dateArray)
 const Calendar = () => {
-  const daysArray = [1, 2, 3, 4, 5, 6, 7]
   return (
     <div>
+      {`${today}`}
       <div>calendar here</div>
-      {daysArray.map((day) => {
+      {/* {dateArray.map((day) => {
         return (
           <div>
             <Day day={day} />
           </div>
         )
-      })}
+      })} */}
     </div>
   )
 }
