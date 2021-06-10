a631550b00ba6b8a1321aeb5d56e5ccf9b38f7b6
Luke Arenas <lukedarenas@gmail.com>
2021-06-10 10:24:10 -0600

    created basic layout of calendar

--- client/src/components/Day.js
+++ client/src/components/Day.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import '../styles/Day.css'
 
 const Day = (props) => {
-  return <div>day {props.day}</div>
+  return (
+    <div className="day-box">
+      <h3>{props.day.date}</h3>
+    </div>
+  )
 }
 
 export default Day
--- client/src/pages/Calendar.js
+++ client/src/pages/Calendar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Day from '../components/Day'
+import '../styles/Calendar.css'
 
 //logic to find current month, year
 const today = new Date().toDateString()
@@ -8,47 +9,76 @@ const month = parseInt(date.slice(5, 7))
 const year = today.slice(11)
 //populating array with date objects
 let dateArray = []
-let n
-const thirtyDayMonths = [4, 6, 9, 11]
-const thirtyOneDayMonths = [1, 3, 5, 7, 8, 10, 12]
-if (thirtyDayMonths.indexOf(month)) {
-  n = 31
-} else if (thirtyOneDayMonths.indexOf(month)) {
-  n = 32
-} else {
-  n = 29
+const placeholderDay = {
+  weekday: '',
+  date: '',
+  month: '',
+  year: ''
 }
-for (let i = 1; i < n; i++) {
+//populating empty days as placeholders for correct weekday placement
+const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+const firstWeekday = new Date(year, month - 1, 1).toString().slice(0, 3)
+let j = weekdays.indexOf(firstWeekday)
+while (j > 0) {
+  dateArray.push(placeholderDay)
+  j--
+}
+//populating with days in month
+let i = 1
+let isContinued = true
+while (isContinued) {
   const nextDay = new Date(year, month - 1, i).toString()
+  const nextDayJSON = new Date(year, month - 1, i).toJSON()
   let dayObj = {
     weekday: nextDay.slice(0, 3),
     date: nextDay.slice(8, 10),
     month: month,
     year: year
   }
-  dateArray.push(dayObj)
+  if (parseInt(nextDayJSON.slice(5, 7)) !== month) {
+    isContinued = false
+  } else {
+    dateArray.push(dayObj)
+    i++
+  }
 }
-//BETTER WAY TO DO IT:
-//let i
-//while continue = true
-//all the date logic above
-//i++
-//if(nextDay.slice(5, 7) !== month)
-//continue = false
 
 console.log(dateArray)
 const Calendar = () => {
   return (
-    <div>
-      {`${today}`}
-      <div>calendar here</div>
-      {/* {dateArray.map((day) => {
-        return (
-          <div>
-            <Day day={day} />
-          </div>
-        )
-      })} */}
+    <div className="calendar">
+      <div className="weekdays">
+        <div className="weekday">
+          <h2>Sunday</h2>
+        </div>
+        <div className="weekday">
+          <h2>Monday</h2>
+        </div>
+        <div className="weekday">
+          <h2>Tuesday</h2>
+        </div>
+        <div className="weekday">
+          <h2>Wednesday</h2>
+        </div>
+        <div className="weekday">
+          <h2>Thursday</h2>
+        </div>
+        <div className="weekday">
+          <h2>Friday</h2>
+        </div>
+        <div className="weekday">
+          <h2>Saturday</h2>
+        </div>
+      </div>
+      <div className="calendar-days">
+        {dateArray.map((day) => {
+          return (
+            <div>
+              <Day day={day} />
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
